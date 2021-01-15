bf7412cddb67d6d2dfd7afec6ed15be2daf0d343
Daniel Espinosa <despinosa@ingeneo.local>
2021-01-15 10:42:12 -0500

    Replace Proj

--- csharp_dotnetcore/16.proactive-messages/Bots/ProactiveBot.cs
+++ csharp_dotnetcore/16.proactive-messages/Bots/ProactiveBot.cs
@@ -1,12 +1,15 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace Microsoft.BotBuilderSamples
 {
@@ -54,10 +57,60 @@ namespace Microsoft.BotBuilderSamples
 
             var text = turnContext.Activity.Text.Trim().ToLower();
 
+            
+            var clientLogin = new RestClient("http://35.185.64.146:80/ingeneo/massive/login");
+            clientLogin.Timeout = -1;
+            var requestLogin = new RestRequest(Method.POST);
+            requestLogin.AddHeader("Content-Type", "application/json");
+            requestLogin.AddParameter("application/json", "{\r\n\"username\": \"daniel.espinosa@ingeneo.com.co\",\r\n\"password\": \"daniel.espinosa@ingeneo.com.co\"\r\n}\r\n", ParameterType.RequestBody);
+            IRestResponse responseLogin = clientLogin.Execute(requestLogin);
+
+            Login token = JsonConvert.DeserializeObject<Login>(responseLogin.Content);
+            Message message = new Message();
+            message.id = DateTime.Now.Ticks.ToString();
+            message.did = "teamsDemo@ingeneo";
+            message.msisdn = turnContext.Activity.Conversation.Id;
+            message.name = turnContext.Activity.From.Name;
+            message.type = "text";
+            message.channel = "WEBCHAT";
+            message.content = text;
+            message.isAttachment = false;
+
+            var clientMessage = new RestClient("http://35.185.64.146:80/ingeneo/massive/inbound");
+            clientMessage.Timeout = -1;
+            var requestMessage = new RestRequest(Method.POST);
+            requestMessage.AddHeader("Authorization", "Bearer " + token.access_token);
+            requestMessage.AddHeader("Content-Type", "application/json");
+            requestMessage.AddParameter("application/json", JsonConvert.SerializeObject(message), ParameterType.RequestBody);
+            IRestResponse responseMessage = clientMessage.Execute(requestMessage);
+
             // Echo back what the user said
-            await turnContext.SendActivityAsync(MessageFactory.Text($"Estamos en proceso de construcción," +
-              $" muy pronto te podras comunicar con bots y agentes de CHATTIGO, Escribiste: {text}."),
-              cancellationToken);
+            //await turnContext.SendActivityAsync(MessageFactory.Text($"Estamos en proceso de construcción," +
+            //  $" muy pronto te podras comunicar con bots y agentes de CHATTIGO, Escribiste: {text}."),
+            //  cancellationToken);
         }
+    }
+    public class Login
+    {
+        public string access_token { get; set; }
+    }
+
+    public class Attachment
+    {
+        public string mediaUrl { get; set; }
+        public string mimeType { get; set; }
+    }
+
+    public class Message
+    {
+        public string id { get; set; }
+        public string did { get; set; }
+        public string msisdn { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string channel { get; set; }
+        public string content { get; set; }
+        public bool isAttachment { get; set; }
+        public Attachment attachment { get; set; }
     }
 }
---
+++ csharp_dotnetcore/16.proactive-messages/Controllers/WebhookController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProactiveBot.Controllers
+{
+    [Route("api/webhook/message")]
+    [ApiController]
+    public class WebhookController : ControllerBase
+    {
+        private readonly IBotFrameworkHttpAdapter _adapter;
+        private readonly string _appId;
+        private readonly ConcurrentDictionary<string, ConversationReference> _conversationReferences;
+
+        public Message _agentMessage { get; set; }
+
+        public WebhookController(IBotFrameworkHttpAdapter adapter, IConfiguration configuration, ConcurrentDictionary<string, ConversationReference> conversationReferences)
+        {
+            _adapter = adapter;
+            _conversationReferences = conversationReferences;
+            _appId = configuration["MicrosoftAppId"];
+
+            // If the channel is the Emulator, and authentication is not in use,
+            // the AppId will be null.  We generate a random AppId for this case only.
+            // This is not required for production, since the AppId will have a value.
+            if (string.IsNullOrEmpty(_appId))
+            {
+                _appId = Guid.NewGuid().ToString(); //if no AppId, use a random Guid
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Message agentMessage)
+        {
+            _agentMessage = agentMessage;
+            foreach (var conversationReference in _conversationReferences.Values)
+            {
+                if(conversationReference.Conversation.Id == agentMessage.msisdn)
+                    await ((BotAdapter)_adapter).ContinueConversationAsync(_appId, conversationReference, BotCallback, default(CancellationToken));
+            }
+
+            // Let the caller know proactive messages have been sent
+            return new ContentResult()
+            {
+                Content = "<html><body><h1>Proactive messages have been sent.</h1></body></html>",
+                ContentType = "text/html",
+                StatusCode = (int)HttpStatusCode.OK,
+            };
+        }
+
+        private async Task BotCallback(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            // If you encounter permission-related errors when sending this message, see
+            // https://aka.ms/BotTrustServiceUrl
+            await turnContext.SendActivityAsync(_agentMessage.content);
+        }
+    }
+
+    public class Attachment
+    {
+        public string mediaUrl { get; set; }
+        public string mimeType { get; set; }
+    }
+
+    public class Message
+    {
+        public string id { get; set; }
+        public string did { get; set; }
+        public string msisdn { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string channel { get; set; }
+        public string content { get; set; }
+        public bool isAttachment { get; set; }
+        public Attachment attachment { get; set; }
+    }
+}
